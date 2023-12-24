88db5c1f5fc4216780ff5abbcdea1757d3d1a105
Destcable <artmobpavlov21@gmail.com>
2023-12-24 18:08:37 +0300

    [upd]

--- editor/src/components/Sidebar/Sidebar.jsx
+++ editor/src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
 import SideCard from "./SideCard";
 import { FaHatWizard } from 'react-icons/fa6';
+import { MdTopic } from "react-icons/md";
 import {
     Col,
     Container,
     Row,
   } from 'react-bootstrap';
 import CanvasHeader from "../Offcanvas/Elements/CanvasHeader";
+import CanvasTopics from "../Offcanvas/Elements/CanvasTopics";
+
 const Sidebar = () => {
     const [showHeader, setShowHeader] = useState(false);
-
+    const [showTopics, setShowTopics] = useState(false);
     return (
         <Container>
             <Row>
@@ -19,6 +22,11 @@ const Sidebar = () => {
                         icon={<FaHatWizard size={'70px'} />}
                         title='Шапка сайта'
                         onClick={() => setShowHeader(true)}
+                    />
+                    <SideCard
+                        icon={<MdTopic size={'70px'} />}
+                        title='Настройка тем'
+                        onClick={() => setShowTopics(true)}
                     />
                     <ul className="list-unstyled">
                         <li>About</li>
@@ -34,6 +42,7 @@ const Sidebar = () => {
                 </Col>
             </Row>
             <CanvasHeader show={showHeader} setShow={setShowHeader}/>
+            <CanvasTopics show={showTopics} setShow={setShowTopics}/>
         </Container>
     )
 };
--- editor/src/index.js
+++ editor/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css'
 import Sidebar from './components/Sidebar/Sidebar';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
