a5dd5b0e530bf3be6a3abdb11c1907d966680c61
Destcable <artmobpavlov21@gmail.com>
2024-02-08 20:04:14 +0300

    [upd]

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,10 +1,32 @@
 async function createTopic(root, args, context) { 
     const { name } = args;
-    return await context.prisma.topic.create({ 
+
+    const templateHeaderSettings = {
+        title: "Title",
+        topic: "",
+        topLineColor: "#3b5571",
+        bottomLineColor: "#43698f",
+        isButton_InfoContent: false,
+        buttonInfoContent_text: "",
+        isButton_KeyWords: false,
+        buttonKeyWords_text: ""
+    };
+
+    const topicData = await context.prisma.topic.create({ 
         data: { 
             name
         }
-    })
+    });
+
+    await context.prisma.topicHeader.create({ 
+        data: { 
+            topicId: topicData.id,
+            settings: templateHeaderSettings
+        }
+    });
+
+
+    return topicData;
 };
 
 async function deleteTopic(root, args, context) { 
