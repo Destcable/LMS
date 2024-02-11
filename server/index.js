bb5a2474a321723219c8bcebca3467901ebde125
Destcable <artmobpavlov21@gmail.com>
2024-02-11 11:40:40 +0300

    [upd]

--- server/index.js
+++ server/index.js
@@ -1,22 +1,15 @@
 const { ApolloServer } = require('apollo-server');
 const { PrismaClient } = require('@prisma/client');
-const path = require('path');
 const Query = require('./src/resolvers/Query');
 const Mutation = require('./src/resolvers/Mutation');
 const { readAndConcatenateSchemas } = require('./src/utils/readAndConcatenateSchemas');
+const { schemaFilePaths } = require('./schemaFilePaths');
 
 const resolvers = { 
     Query,
     Mutation
 };
 
-const schemaFilePaths = [
-    path.join(__dirname, 'customTypes.graphql'),
-    path.join(__dirname, 'src/models/Topic/schema.graphql'),
-    path.join(__dirname, 'src/models/TopicHeader/schema.graphql'),
-    path.join(__dirname, 'src/models/TopicTheme/schema.graphql'),
-    path.join(__dirname, 'src/models/ThemeTask/schema.graphql'),
-];
 const prisma = new PrismaClient();
 
 const server = new ApolloServer({
