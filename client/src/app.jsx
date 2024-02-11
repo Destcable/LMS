f1332b027c739ec3581c193b0e4a3deff08006bf
Destcable <artmobpavlov21@gmail.com>
2024-02-11 23:11:11 +0300

    [upd]

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -4,7 +4,6 @@ import SelectThemeContainer from "./containers/SelectThemeContainer";
 import getStorageAuth from "./services/getStorageAuth";
 import authService from "./services/authService";
 import MainThemeUI from "./ui/MainTheme/MainThemeUI";
-import { handleSelectTheme } from "./handlers/themeHandler";
 import WindowContainer from "./containers/WindowContainer";
 
 const App = () => { 
@@ -22,7 +21,7 @@ const App = () => {
     
         if (authService.login(login, password)) { 
             return <SelectThemeContainer 
-                onThemeSelected={(data) => handleSelectTheme(data, setSelectableTopic)} 
+                onThemeSelected={data => setSelectableTopic(data)} 
             />
         }
 
--- client/src/handlers/themeHandler.js
+++
@@ -1,8 +0,0 @@
-import useQueryTopicHeader from "../hooks/useQueryTopicHeader";
-
-const handleSelectTheme = (theme, setSelectableTheme) => { 
-    setSelectableTheme(theme)
-};
-
-
-export { handleSelectTheme };
