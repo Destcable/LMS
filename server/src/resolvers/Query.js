a335090d4db06c3bfd56860c32cddd49570fd1b4
Destcable <artmobpavlov21@gmail.com>
2024-02-11 11:01:23 +0300

    [upd]

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,30 +1,6 @@
-async function topics(_, _, context) {
-    return await context.prisma.topic.findMany();
-};
-
-async function topicHeader(_, args, context) {
-    const { topicId } = args;
-
-    return await context.prisma.topicHeader.findUnique({
-        where: { topicId }
-    });
-};
-
-async function topicThemes(_, args, context) { 
-    const { topicId } = args;
-
-    return await context.prisma.topicTheme.findMany({
-        where: { topicId }
-    })
-};
-
-async function topicThemeById(_, args, context) {
-    const { id } = args;
-
-    return await context.prisma.topicTheme.findUnique({
-        where: { id }
-    });
-};
+const { topics } = require("../models/Topic/resolvers/Query");
+const { topicHeader } = require("../models/TopicHeader/resolvers/Query");
+const { topicThemes, topicThemeById } = require("../models/TopicTheme/resolvers/Query");
 
 module.exports = { 
     topics, 
